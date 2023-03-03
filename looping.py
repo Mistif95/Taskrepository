a74aac416c8789ca97adcb371730db4a5bf6925b
Mistif95 <rayhn.wibowo710@gmail.com>
2023-03-03 15:54:38 +0700

    Looping cek bilangan jika ganjil dengan penambahan function (v.1.2)

--- looping.py
+++ looping.py
@@ -1,4 +1,10 @@
-# Versi 1.1
+# Versi 1.2
+def check_even_odd(num):
+    if num % 2 == 1:
+        return True
+    else:
+        return False
+
 for i in range(1, 101):
-    if i % 2 == 1:
+    if check_even_odd(i):
         print(i)
